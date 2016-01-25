6c4a9901836ec4bceb19f0c8bed3091057d91d2a
elpenao <elpenao@gmail.com>
2016-01-25 17:04:40 -0500

    setup

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require( 'express' );
+var swig = require('swig');
+var routes = require('./routes/');
+var bodyParser = require('body-parser');
+// var socketio = require('socket.io');
+
+var app = express(); 
+var server = app.listen(3000);
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
